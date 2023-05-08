e352f455aa95f13edc5955d13db0086bdd6d2b33
MerewareStudios <77405540+MerewareStudios@users.noreply.github.com>
2023-05-08 10:10:55 +0300

    Initial commit

---
+++ Game Project/Assets/Auto Generated/Generated/Particle.cs
@@ -0,0 +1,19 @@
+public enum Particle
+{
+    Leaf_Explosion = 0,
+    Leaf_Sparks = 1,
+    Debris = 2,
+    Grass_Explosion = 3,
+    Blood_Explosion = 4,
+    Droplets = 5,
+    Apple_Debris = 6,
+    Iron_Debris = 7,
+    Particle_AppleTree = 8,
+    Particle_Cow = 9,
+    Particle_Crops = 10,
+    Particle_Crystal = 11,
+    Particle_Stone = 12,
+    Particle_Wood = 13,
+    HealBig = 14,
+
+}
