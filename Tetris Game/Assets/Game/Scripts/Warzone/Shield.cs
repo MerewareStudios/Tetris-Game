ac83e5d268da0fb9296b18796b27e272403696d2
Orkun Sarız <merewarestudios@gmail.com>
2023-09-07 22:11:33 +0300

    Bup

--- Tetris Game/Assets/Game/Scripts/Warzone/Shield.cs
+++ Tetris Game/Assets/Game/Scripts/Warzone/Shield.cs
@@ -1,43 +1,29 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using DG.Tweening;
-using Game;
-using Internal.Core;
 using UnityEngine;
 
-public class Shield : MonoBehaviour, TickManager.ITickable
+public class Shield : MonoBehaviour
 {
-    [SerializeField] private MeshRenderer frontLineMR;
     [SerializeField] private ParticleSystem zonePs;
     [SerializeField] private Vector3 targetShowScale;
     [SerializeField] private Vector3 targetHideScale;
-    [System.NonSerialized] private bool _activated = false;
     [System.NonSerialized] private Data _data;
-    [System.NonSerialized] private Tween delayTween;
-    [System.NonSerialized] private float _lastTimeTicked;
 
     private bool Enabled
     {
         set
         {
-            if (_activated == value)
+            if (zonePs.isPlaying == value)
             {
                 return;
             }
-            _activated = value;
             if (value)
             {
                 zonePs.Play();
                 zonePs.transform.DOKill();
                 zonePs.transform.DOScale(targetShowScale, 0.35f).SetEase(Ease.OutBack);
-                
-                frontLineMR.SetGradient(0.0f, 1.0f, 0.5f, GameManager.MPB_FRONT, GameManager.BaseColor, Const.THIS.frontLineGradient);
             }
             else
             {
-                frontLineMR.SetGradient(1.0f, 0.0f, 0.5f, GameManager.MPB_FRONT, GameManager.BaseColor, Const.THIS.frontLineGradient);
-
                 zonePs.transform.DOKill();
                 zonePs.transform.DOScale(targetHideScale, 0.35f).SetEase(Ease.InBack).onComplete += () =>
                 {
@@ -45,7 +31,7 @@ public class Shield : MonoBehaviour, TickManager.ITickable
                 };
             }
         }
-        get => _activated;
+        get => zonePs.isPlaying;
     }
     
     public Data _Data
@@ -64,7 +50,6 @@ public class Shield : MonoBehaviour, TickManager.ITickable
     public void AddShield(int amount)
     {
         _Data.amount += amount;
-        _Data.SetTime(Const.THIS.shieldMaxDuration);
         _Data = _data;
     }
     public int ConsumeShield(int amount)
@@ -88,27 +73,25 @@ public class Shield : MonoBehaviour, TickManager.ITickable
     }
     public void ResumeProtection()
     {
-        _lastTimeTicked = Time.time;
         if (_data.CanProtect)
         {
             DisplayProtection();
-            if (!TickManager.THIS.IsTicking(this))
-            {
-                TickManager.THIS.AddTickable(this);
-            }
+            // if (!TickManager.THIS.IsTicking(this))
+            // {
+            //     TickManager.THIS.AddTickable(this);
+            // }
         }
     }
     public void PauseProtection()
     {
-        TickManager.THIS.RemoveTickable(this);
+        // TickManager.THIS.RemoveTickable(this);
     }
 
     public float TickInterval { get => StatDisplayArranger.UpdateInterval;}
 
     public void OnTick()
     {
-        _Data.ConsumeTime(Time.time - _lastTimeTicked);
-        _lastTimeTicked = Time.time;
+        // _Data.ConsumeTime(Time.time - _lastTimeTicked);
         
         StatDisplayArranger.THIS.Show(StatDisplay.Type.Shield, _Data.amount, _Data.Percent, false, false);
 
@@ -121,28 +104,28 @@ public class Shield : MonoBehaviour, TickManager.ITickable
         
         Enabled = false;
             
-        TickManager.THIS.RemoveTickable(this);
+        // TickManager.THIS.RemoveTickable(this);
         StatDisplayArranger.THIS.Hide(StatDisplay.Type.Shield);
     }
 
     
     [System.Serializable]
-    public class Data : ICloneable
+    public class Data : System.ICloneable
     {
-        [SerializeField] public float maxTime = 0.0f;
+        [System.NonSerialized] public float maxTime = 0.0f;
         [SerializeField] public float timeLeft = 0.0f;
         [SerializeField] public int amount;
 
-        public Data()
-        {
-            
-        }
-        public Data(float timeLeft, float maxTime, int amount)
-        {
-            this.timeLeft = timeLeft;
-            this.maxTime = maxTime;
-            this.amount = amount;
-        }
+        // public Data()
+        // {
+        //     
+        // }
+        // public Data(float timeLeft, int amount)
+        // {
+        //     this.timeLeft = timeLeft;
+        //     this.maxTime = this.timeLeft;
+        //     this.amount = amount;
+        // }
 
         public float Percent => timeLeft / maxTime;
         public bool CanProtect => AvailableByTime && amount > 0;
@@ -153,19 +136,14 @@ public class Shield : MonoBehaviour, TickManager.ITickable
             timeLeft += duration;
             maxTime = timeLeft;
         }
-        public void SetTime(float duration)
-        {
-            timeLeft = duration;
-            maxTime = timeLeft;
-        }
         public void ConsumeTime(float duration)
         {
-            timeLeft = Mathf.Clamp(timeLeft - duration, 0.0f, float.MaxValue);
+            timeLeft = Mathf.Max(timeLeft - duration, 0.0f);
         }
         public Data(Shield.Data data)
         {
             this.timeLeft = data.timeLeft;
-            this.maxTime = data.maxTime;
+            this.maxTime = this.timeLeft;
             this.amount = data.amount;
         }
 
