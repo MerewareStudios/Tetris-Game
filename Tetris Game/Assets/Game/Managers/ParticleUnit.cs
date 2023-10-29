be776692eeb1257fe2a71694b10f5b75ef6e3362
Orkun Sarız <merewarestudios@gmail.com>
2023-10-29 21:38:07 +0300

    Mesh anim bup

--- Tetris Game/Assets/Game/Managers/ParticleUnit.cs
+++ Tetris Game/Assets/Game/Managers/ParticleUnit.cs
@@ -44,7 +44,6 @@ public class ParticleUnit : MonoBehaviour
     
     void OnParticleSystemStopped()
     {
-        Debug.Log(key.ToString() + " despawn");
         this.Despawn(key);    
     }
 }
