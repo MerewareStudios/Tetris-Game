1c76021adb9bd67f8e0a4751d8efe391cc4c549b
Orkun Sarız <merewarestudios@gmail.com>
2023-11-21 17:10:29 +0300

    App label

--- Tetris Game/Assets/Game/Managers/ApplicationManager.cs
+++ Tetris Game/Assets/Game/Managers/ApplicationManager.cs
@@ -1,26 +1,22 @@
 using System;
-using System.Collections;
-using System.Diagnostics;
 using Internal.Core;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
-using Debug = UnityEngine.Debug;
 
 public class ApplicationManager : Singleton<ApplicationManager>
 {
     [SerializeField] public bool multiTouchEnabled = false;
     [SerializeField] public bool useNativeFrameRate = true;
     [SerializeField] public int targetFrameRate = 60;
-    [SerializeField] public TextMeshProUGUI fpsText;
     [SerializeField] private ScriptableRendererFeature grabTextureFeature;
-    [System.NonSerialized] public int fps;
-    [System.NonSerialized] public float fpsTimestamp;
-    // static Setting HAPTIC;
-    // public static Setting SOUND;
-
-    // private long _elapsed;
-    // private Stopwatch _stopwatch;
+    [SerializeField] public GameObject appLabel;
+    [SerializeField] public TextMeshProUGUI fpsText;
+    
+#if FPS
+    [System.NonSerialized] private int _fps;
+    [System.NonSerialized] private float _fpsTimestamp;
+#endif
 
     public bool GrabFeatureEnabled
     {
@@ -30,19 +26,47 @@ public class ApplicationManager : Singleton<ApplicationManager>
 
     public virtual void Awake()
     {
-        // _stopwatch = new Stopwatch();
-        // _stopwatch.Start();
-        
         Input.multiTouchEnabled = multiTouchEnabled;
         
 #if !(DEVELOPMENT_BUILD || UNITY_EDITOR)
      Debug.unityLogger.logEnabled = false; 
 #endif
         Application.targetFrameRate = useNativeFrameRate ? (int)Screen.currentResolution.refreshRateRatio.value : targetFrameRate;
-        // LoadSettings();
-        fpsTimestamp = Time.realtimeSinceStartup;
+        
+#if FPS
+        _fpsTimestamp = Time.realtimeSinceStartup;
+        appLabel.SetActive(true);
+#else
+        appLabel.SetActive(false);
+#endif
+        
+        
         GrabFeatureEnabled = false;
     }
+    
+#if FPS
+    void LateUpdate()
+    {
+        _fps++;
+        if (Time.realtimeSinceStartup - _fpsTimestamp > 1.0f)
+        {
+            TimeSpan t = TimeSpan.FromSeconds(Time.realtimeSinceStartup);
+
+            string stamp = string.Format("{0}:{1}:{2}", 
+                t.Hours, 
+                t.Minutes, 
+                t.Seconds
+                );
+            
+            
+            fpsText.text = _fps.ToString() + " | " + stamp + " | (" + Application.version + " " + Const.THIS.bundleVersionCode + ")";
+            _fps = 0;
+            _fpsTimestamp = Time.realtimeSinceStartup;
+        }
+    }
+#endif
+    
+        // LoadSettings();
     // private void LoadSettings()
     // {
     //     HAPTIC = new("HAPTIC", false);
@@ -57,34 +81,8 @@ public class ApplicationManager : Singleton<ApplicationManager>
     //    }
     //}
 
-    // IEnumerator Start()
-    // {
-    //     yield return null;
-    //     _elapsed = _stopwatch.ElapsedMilliseconds;
-    //     Debug.Log(_elapsed);
-    // }
-
-    void LateUpdate()
-    {
-        fps++;
-        if (Time.realtimeSinceStartup - fpsTimestamp > 1.0f)
-        {
-            TimeSpan t = TimeSpan.FromSeconds(Time.realtimeSinceStartup);
-
-            string stamp = string.Format("{0}:{1}:{2}", 
-                t.Hours, 
-                t.Minutes, 
-                t.Seconds
-                );
-            
-            
-            fpsText.text = fps.ToString() + " | " + stamp + " | (" + Application.version + " " + Const.THIS.bundleVersionCode + ")";
-            // fpsText.text = fps.ToString() + " | " + stamp + " | (" + Application.version + " " + Const.THIS.bundleVersionCode + ") " + _elapsed;
-            // fpsText.text = fps.ToString() + " | " + stamp + " | " + Helper.ScreenDPI() + " | " + Helper.ScreenHeightDP();
-            fps = 0;
-            fpsTimestamp = Time.realtimeSinceStartup;
-        }
-    }
+    // static Setting HAPTIC;
+    // public static Setting SOUND;
 
     // public class Setting
     // {
