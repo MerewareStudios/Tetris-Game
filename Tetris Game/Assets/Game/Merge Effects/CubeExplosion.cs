1f9f33ea10a70b9852b9508f293e37f9139dceeb
Orkun Sarız <merewarestudios@gmail.com>
2023-12-08 06:39:50 +0300

    Merge fix

---
+++ Tetris Game/Assets/Game/Merge Effects/CubeExplosion.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using DG.Tweening;
+using Internal.Core;
+using UnityEngine;
+
+public class CubeExplosion : MonoBehaviour
+{
+    [SerializeField] private List<Transform> fragments;
+    [SerializeField] private List<Rigidbody> rbs;
+
+    public void Explode(Vector3 position)
+    {
+        int index = 0;
+        transform.SetPositionAndRotation(position, Random.rotation);
+        for (int i = 0; i < fragments.Count; i++)
+        {
+            Transform fragment = fragments[i];
+            
+            fragment.DOKill();
+
+            Vector3 direction = Random.insideUnitSphere.normalized;
+            
+            fragment.localScale = AnimConst.THIS.fragmentScale;
+            fragment.localPosition = AnimConst.THIS.explodeRadius * direction;
+            fragment.rotation = Random.rotation;
+            
+            fragment.DOScale(new Vector3(0.1f, 0.1f, 0.1f), 0.4f).SetUpdate(UpdateType.Fixed).SetEase(Ease.InOutSine).SetDelay(Random.Range(0.35f, 0.5f)).onComplete = () =>
+            {
+                index++;
+                if (index == fragments.Count)
+                {
+                    // this.Despawn(Pool.Cube_Explosion);
+                }
+            };
+
+            Rigidbody rb = rbs[i];
+            rb.velocity = direction * (AnimConst.THIS.explodePower * Random.Range(0.5f, 1.0f)) + new Vector3(0.0f, AnimConst.THIS.jumpPower * (fragment.position.y - position.y), 0.0f);
+            rb.angularVelocity = Random.insideUnitSphere * 20.0f;
+        }
+    }
+}
