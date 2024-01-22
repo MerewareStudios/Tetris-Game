8752204195aae9611d4200ac6986ca173d3468f8
Orkun Sarız <merewarestudios@gmail.com>
2024-01-22 09:15:05 +0300

    Random block

--- Tetris Game/Assets/Game/Managers/SaveManager.cs
+++ Tetris Game/Assets/Game/Managers/SaveManager.cs
@@ -97,7 +97,7 @@ public static class SaveManagerExtensions
     }
     public static Pool RandomBlock(this Spawner spawner)
     {
-        return Pool.O_Block;
+        // return Pool.O_Block;
         return SaveManager.THIS.SaveData.userData.blockShopData.GetRandomBlock();
     }
     public static int CurrentLevel(this LevelManager levelManager)
