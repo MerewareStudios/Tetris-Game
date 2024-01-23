477e85a65d32b325b83bd1edb4f08f40014f967b
Orkun Sarız <merewarestudios@gmail.com>
2024-01-24 00:34:45 +0300

    Audio opt

--- Tetris Game/Assets/Game/Managers/AudioManager.cs
+++ Tetris Game/Assets/Game/Managers/AudioManager.cs
@@ -1,8 +1,6 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor.AddressableAssets.Settings;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.Audio;
